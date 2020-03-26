e3493aab68289494ac62f272d4ec0f52f86eb2b4
Tushar <tushartuteja.india@gmail.com>
2020-03-26 21:31:21 +0530

    Juint commit

---
+++ src/main/java/com/example/Junit.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Junit {
+        public String removeCharacter(String str) {
+            String r = str;
+
+            if(str.length()<=2 && str.contains("A"))
+                r = str.replace("A","");
+            else if(str.contains("A")) {
+                String start = str.substring(0,2);
+                String end = str.substring(2,str.length());
+                r = start.replace("A", "") + end;
+            }
+            return r;
+        }
+}
